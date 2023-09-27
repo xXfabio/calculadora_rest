4babf44df2ede911b6ee95ea4ea3ff40a38de6ce
fabiolobo <fabiolobolima@gmail.com>
2023-09-27 14:17:35 -0300

    PRIMEIRO

---
+++ src/main/java/br/com/lobo/calculadora/exeptions/ExceptionResponse.java
@@ -0,0 +1,25 @@
+package br.com.lobo.calculadora.exeptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    
+    
+}
